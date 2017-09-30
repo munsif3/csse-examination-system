343da10019928c5baa50f8543369ef7f94212e9c
Saranki M <m.saranki@gmail.com>
2017-09-30 13:42:34 +0530

    Added common methods to validate textfields.

--- src/com/csse/exam/common/Validation.java
+++ src/com/csse/exam/common/Validation.java
@@ -9,76 +9,115 @@ import com.csse.exam.config.DBConnection;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 import com.csse.exam.model.User;
+import java.awt.Component;
 import java.sql.ResultSet;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
  * @author User
  */
 public class Validation {
-    
+
     Connection conn = null;
     User user;
-    
-        //to check the login credentials 
-        public boolean validateLogin(String username,String password) {
-            try{              
-                 conn = (Connection) DBConnection.getConnection();
-                PreparedStatement pst = (PreparedStatement) conn.prepareStatement("Select * from user where username=? and userPassword=?");
-                pst.setString(1, username); 
-                pst.setString(2, password);
-                ResultSet rs = pst.executeQuery(); 
-                
-                if(rs.next()){
-                    user= new User(rs.getString("userId"),rs.getString("name"),rs.getString("role"),rs.getString("username"),rs.getString("userPassword"));
+
+    //to check the login credentials 
+    public boolean validateLogin(String username, String password) {
+        try {
+            conn = (Connection) DBConnection.getConnection();
+            PreparedStatement pst = (PreparedStatement) conn.prepareStatement("Select * from user where username=? and userPassword=?");
+            pst.setString(1, username);
+            pst.setString(2, password);
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+                user = new User(rs.getString("userId"), rs.getString("name"), rs.getString("role"), rs.getString("username"), rs.getString("userPassword"));
 //                    JOptionPane.showMessageDialog(null, "Name is" +rs.getString("name") );
-                    return true;    
-            }else
-                    return false;            
-            }
-            catch(Exception e){
-                e.printStackTrace();
-                return false;
-            }       
-} 
-        
-        public boolean checkTextNull(String text){
-            if(text.length()==0)// Checking for empty field
-            {  
                 return true;
-            }
-            else{
+            } else {
                 return false;
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
-        
-        /**
-     * 
-     * @param number
-     * @return 
-     */
-    public boolean checkNumberBox(String number)
-    {        
-        return (number.matches("[0-9]+"));   
     }
-    
-    /**
-     * 
-     * @param numbers[]
-     * @return 
-     */
-    public boolean checkOverAllNumberBox(String numbers[])
-    {
-        boolean value=true;
-        for(String number : numbers)
+
+    public boolean checkTextNull(String text) {
+        if (text.length() == 0)// Checking for empty field
         {
-            value  = checkNumberBox(number);
-            if(value==false)
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param number
+     * @return
+     */
+    public boolean checkNumberBox(String number) {
+        return (number.matches("[0-9]+"));
+    }
+
+    /**
+     *
+     * @param numbers[]
+     * @return
+     */
+    public boolean checkOverAllNumberBox(String numbers[]) {
+        boolean value = true;
+        for (String number : numbers) {
+            value = checkNumberBox(number);
+            if (value == false) {
                 return value;
+            }
         }
         return value;
     }
-        
-        
+
+    public boolean checkTextBox(String text) {
+        return (text.matches("[A-Za-z]+"));
+    }
+
+    /**
+     *
+     * @param text[]
+     * @return
+     */
+    public boolean checkOverAllTextBox(String text[]) {
+        boolean value = true;
+        for (String word : text) {
+            value = checkTextBox(word);
+            if (value == false) {
+                return value;
+            }
+        }
+        return value;
+    }
+
+    /**
+     *
+     * @param panel
+     * @return
+     */
+    public boolean checkEmptyTextBox(JPanel panel) {
+        Component components[] = panel.getComponents();
+        for (Component component : components) {
+            if (component instanceof JTextField) {
+                JTextField textBox = (JTextField) component;
+                System.out.println(textBox.getText());
+
+                if (textBox.getText().equals("")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
