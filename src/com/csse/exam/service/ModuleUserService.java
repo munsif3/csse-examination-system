c54661349cb8a5e44939b237435636fda9fc938d
Sudheesan <surendransudheesan@gmail.com>
2017-11-03 16:39:27 +0530

    commit

--- src/com/csse/exam/service/ModuleUserService.java
+++ src/com/csse/exam/service/ModuleUserService.java
@@ -19,93 +19,103 @@ import javax.swing.JOptionPane;
  * @author User
  */
 public class ModuleUserService {
+
     private String moduleId;
-    
+
     /**
      * This method will validate the module fields
+     *
      * @param moduleName
      * @param password
-     * @return 
+     * @return
      */
-    public boolean validateValues(String moduleName,String password)
-    {
+    public boolean validateValues(String moduleName, String password) {
         Validation validation = new Validation();
         if (validation.checkTextNull(moduleName) || validation.checkTextNull(password)) {
             JOptionPane.showMessageDialog(null, "You can't keep fields empty");
             return false;
-        } 
-        else {
+        } else {
             return true;
         }
     }
-    
+
     /**
      * This method will generate new module id
-     * @return 
+     *
+     * @return
      */
-    public String getNewModuleId(){
+    public String getNewModuleId() {
         String lastModuleId = null;
         String newModuleId = null;
 
         try {
-               
-                Connection con = DBConnection.getConnection();
-                String query = "SELECT moduleId FROM module\n" +
-                                "order by moduleId desc limit 1;";
-                PreparedStatement preparedStmt = (PreparedStatement) con.prepareStatement(query);
-                
-                // execute the preparedstatement
-                ResultSet rs = preparedStmt.executeQuery(); 
-                if(rs.next()){
-                    lastModuleId = rs.getString("moduleId");
-                }
-                String last3 = lastModuleId.substring(lastModuleId.length() - 3);
-                newModuleId = lastModuleId.substring(0,4)+String.valueOf(Integer.parseInt(last3) + 1);
-                
-                con.close();
 
-                } catch (SQLException | HeadlessException e) {
-                    JOptionPane.showMessageDialog(null, e);
-                }
+            Connection con = DBConnection.getConnection();
+            String query = "SELECT moduleId FROM module\n"
+                    + "order by moduleId desc limit 1;";
+            PreparedStatement preparedStmt = (PreparedStatement) con.prepareStatement(query);
+
+            // execute the preparedstatement
+            ResultSet rs = preparedStmt.executeQuery();
+            if (rs.next()) {
+                lastModuleId = rs.getString("moduleId");
+            }
+            String last3 = lastModuleId.substring(lastModuleId.length() - 3);
+            newModuleId = lastModuleId.substring(0, 4) + String.valueOf(Integer.parseInt(last3) + 1);
+
+            con.close();
+
+        } catch (SQLException | HeadlessException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
         return newModuleId;
     }
-    
+
     /**
      * this method will generate allocated year and semester
+     *
      * @param year
      * @param semester
-     * @return 
+     * @return
      */
-    public String getAllocatedYearAndSemester(int year, int semester){
+    public String getAllocatedYearAndSemester(int year, int semester) {
         String yearPrefix = null;
         String semesterPrefix = null;
         String allocatedTo;
-        
-       
-        switch(year)
-        {
-            case(0):yearPrefix="Y1";break;
-            case(1):yearPrefix="Y2";break;
-            case(2):yearPrefix="Y3";break;
-            case(3):yearPrefix="Y4";break;
+
+        switch (year) {
+            case (0):
+                yearPrefix = "Y1";
+                break;
+            case (1):
+                yearPrefix = "Y2";
+                break;
+            case (2):
+                yearPrefix = "Y3";
+                break;
+            case (3):
+                yearPrefix = "Y4";
+                break;
         }
-       switch(semester)
-       {
-           case(0):semesterPrefix="S1";break;
-           case(1):semesterPrefix="S2";break;
-       }
-       
-       allocatedTo = yearPrefix+semesterPrefix;
-       return allocatedTo;
+        switch (semester) {
+            case (0):
+                semesterPrefix = "S1";
+                break;
+            case (1):
+                semesterPrefix = "S2";
+                break;
+        }
+
+        allocatedTo = yearPrefix + semesterPrefix;
+        return allocatedTo;
     }
 
-    
-     public void setModuleID(String id)
-     {
-         moduleId = id;
-     }
-     public String getModuleID(){
-         return moduleId;
-     }
-     
+    public void setModuleID(String id) {
+        moduleId = id;
+    }
+
+    public String getModuleID() {
+        return moduleId;
+    }
+
 }
